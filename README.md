57f4419a9e1d672e3b8e6b1f6a279240f8c74d3d
Homero Hernandez <homero.hernandez0@walmart.com>
2023-05-29 23:20:16 -0400

    adding Readme

---
+++ README.md
@@ -0,0 +1,14 @@
+
+# Golang DDD Archetype
+
+
+
+## Run Example:
+
+	git clone https://github.com/Nitri0/archetype-golang-ddd.git
+
+    cd archetype-golang-ddd/cmd
+
+    go run main.go
+
+
--- cmd/main.go
+++ cmd/main.go
@@ -1,13 +1,29 @@
 package main
 
-import webserver "go-archtype-ddd/internal/infraestructure/web-server"
+import (
+	"go-archtype-ddd/internal/application"
+	stub_persistence "go-archtype-ddd/internal/infraestructure/persistence/stub"
+	webserver "go-archtype-ddd/internal/infraestructure/web-server"
+	gin_webserver "go-archtype-ddd/internal/infraestructure/web-server/gin"
+)
 
 func main() {
-
-	server := NewWebApplication()
+	server := FactoryWebApplication()
 	RunWebServer(server)
 }
 
 func RunWebServer(httpServer webserver.HttpServer) {
 	httpServer.Start("3031")
+}
+
+func FactoryWebApplication() *gin_webserver.GinHttpServer {
+
+	orderRepository := stub_persistence.NewStubOrderRepository()
+	productRepository := stub_persistence.NewStubProductRepository()
+
+	createOrderUseCase := application.NewCreateOrderCommandHandler(orderRepository, productRepository)
+
+	server := gin_webserver.NewGinHttpServer(createOrderUseCase)
+	return server
+
 }
